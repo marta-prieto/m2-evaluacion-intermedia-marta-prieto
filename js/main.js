3bb367c736bb3ce52f04b77f1bba0747016f1c3b
Marta Prieto <martaprietoluque@gmail.com>
2019-07-30 20:57:16 +0200

    fix commets

--- js/main.js
+++ js/main.js
@@ -1,26 +1,45 @@
 'use strict';
 
 
-const field = document.querySelector('input');//app__field
-const containerCounter = document.querySelector('.zero');//app__counter
-const numberZero = document.querySelector('.app__counter');//app__clue
-const button = document.querySelector('.btn');//app__play
+const field = document.querySelector('input');
+const containerCounter = document.querySelector('.zero');
+const numberZero = document.querySelector('.app__counter');
+const button = document.querySelector('.btn');
 
 let counter = 0;
 containerCounter.innerHTML = counter;
 
 
-// Función que genera un número aleatorio hasta un máximo dado
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max)
 }
 
-// Anda, chacho, píntame en la consola un número random hasta 100;
+
 const myRandomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
 
+function tryGame () {
+    const userNumber = parseInt(field.value);
+  
+    if (myRandomNumber === userNumber) {
+        numberZero.innerHTML = '¡Has ganado campeona!';
+    
+    } else if (myRandomNumber > userNumber) {
+        numberZero.innerHTML = 'Demasiado bajo';
+    
+    } else {
+        numberZero.innerHTML = 'Demasiado alto';
+        counter += 1;
+        containerCounter.innerHTML = counter;
+    }
+
+}
+
+
+button.addEventListener('click', tryGame);
 
 
 
