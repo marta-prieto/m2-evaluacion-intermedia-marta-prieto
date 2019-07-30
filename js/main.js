71634fc678fc5c1f4a038ac6ec250a8adb579d3b
Marta Prieto <martaprietoluque@gmail.com>
2019-07-30 20:52:08 +0200

    fix html and add js const

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 'use strict';
 
 
-const input = document.querySelector('input');
-const guess = document.querySelector('.guess');
-const text = document.querySelector('.text');
-const number = document.querySelector('.counter');
-const btn = document.querySelector('.btn');
-const title = document.querySelector('.title');
+const field = document.querySelector('input');//app__field
+const containerCounter = document.querySelector('.zero');//app__counter
+const numberZero = document.querySelector('.app__counter');//app__clue
+const button = document.querySelector('.btn');//app__play
 
-function write(addEventListener) {
-    const add = addEventListener.currentTarget.value;
-    btn.innerHTML = add;
-}
-function getRandomNumber(addEventListener) {
-    const number = addEventListener.currentTarget.value;
-    number.innerHTML = add;
-}
+let counter = 0;
+containerCounter.innerHTML = counter;
+
 
 // Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
@@ -27,9 +20,10 @@ const myRandomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
-function number() {
-    counter++ , counterBox.innerHTML = counter
-}
-input.addEventListener('keyup', write)
-btn.addEventListener('click', getRandomNumber)
-counter.addEventListener('click', number)
+
+
+
+
+
+
+
