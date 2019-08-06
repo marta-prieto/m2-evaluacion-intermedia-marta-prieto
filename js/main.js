036a4c95c43432fde083899e57e9f85be2c41a17
Marta Prieto <martaprietoluque@gmail.com>
2019-08-06 19:47:39 +0200

    add elements

--- js/main.js
+++ js/main.js
@@ -9,27 +9,30 @@ const button = document.querySelector('.btn');
 let counter = 0;
 containerCounter.innerHTML = counter;
 
-
-
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max)
 }
 
-
 const myRandomNumber = getRandomNumber(100);
-
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
 
-function tryGame () {
+function tryGame() {
     const userNumber = parseInt(field.value);
-  
+
     if (myRandomNumber === userNumber) {
         numberZero.innerHTML = '¡Has ganado campeona!';
-    
+
     } else if (myRandomNumber > userNumber) {
         numberZero.innerHTML = 'Demasiado bajo';
-    
+        counter += 1;
+        containerCounter.innerHTML = counter;
+
+    } else if (userNumber > 100) {
+        numberZero.innerHTML = '¡Error! Es un número menor que 100';
+        counter += 1;
+        containerCounter.innerHTML = counter;
+
     } else {
         numberZero.innerHTML = 'Demasiado alto';
         counter += 1;
@@ -43,6 +46,11 @@ button.addEventListener('click', tryGame);
 
 
 
+/* const resetButton = document.querySelector('.js__button');
 
+function resetInput() {
+    numberZero.innerHTML = defaultElement.value;
+}
 
+resetButton.addEventListener('click', resetInput); */
 
